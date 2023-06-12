23a7e64c9a38013fcf7d8433d060677e988b68bc
unknown <evelinemariavizante@gmail.com>
2023-06-12 12:35:08 +0200

    test

--- travel-main/Reisebüro_SC/Reisebüro_SC/Form1.Designer.cs
+++ travel-main/Reisebüro_SC/Reisebüro_SC/Form1.Designer.cs
@@ -34,21 +34,21 @@
             this.Place = new System.Windows.Forms.TextBox();
             this.HotelName = new System.Windows.Forms.TextBox();
             this.Price = new System.Windows.Forms.TextBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.HotelName1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Place1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Price1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // TravelID
             // 
             this.TravelID.BackColor = System.Drawing.Color.Teal;
-            this.TravelID.Location = new System.Drawing.Point(37, 354);
+            this.TravelID.Location = new System.Drawing.Point(44, 258);
+            this.TravelID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TravelID.Name = "TravelID";
-            this.TravelID.Size = new System.Drawing.Size(206, 48);
+            this.TravelID.Size = new System.Drawing.Size(183, 38);
             this.TravelID.TabIndex = 0;
             this.TravelID.Text = "Zur Buchung";
             this.TravelID.UseVisualStyleBackColor = false;
@@ -58,9 +58,10 @@
             // 
             this.Picture.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.Picture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Picture.Location = new System.Drawing.Point(37, 80);
+            this.Picture.Location = new System.Drawing.Point(44, 26);
+            this.Picture.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Picture.Name = "Picture";
-            this.Picture.Size = new System.Drawing.Size(206, 143);
+            this.Picture.Size = new System.Drawing.Size(183, 115);
             this.Picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Picture.TabIndex = 2;
             this.Picture.TabStop = false;
@@ -68,10 +69,11 @@
             // Place
             // 
             this.Place.BackColor = System.Drawing.Color.Teal;
-            this.Place.Location = new System.Drawing.Point(37, 261);
+            this.Place.Location = new System.Drawing.Point(44, 184);
+            this.Place.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Place.Multiline = true;
             this.Place.Name = "Place";
-            this.Place.Size = new System.Drawing.Size(206, 46);
+            this.Place.Size = new System.Drawing.Size(184, 38);
             this.Place.TabIndex = 4;
             this.Place.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.Place.TextChanged += new System.EventHandler(this.Reisebüro_Load);
@@ -79,75 +81,68 @@
             // HotelName
             // 
             this.HotelName.BackColor = System.Drawing.Color.Teal;
-            this.HotelName.Location = new System.Drawing.Point(37, 229);
+            this.HotelName.Location = new System.Drawing.Point(44, 158);
+            this.HotelName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.HotelName.Name = "HotelName";
-            this.HotelName.Size = new System.Drawing.Size(206, 26);
+            this.HotelName.Size = new System.Drawing.Size(184, 22);
             this.HotelName.TabIndex = 7;
             this.HotelName.TextChanged += new System.EventHandler(this.Reisebüro_Load);
             // 
             // Price
             // 
             this.Price.BackColor = System.Drawing.Color.Teal;
-            this.Price.Location = new System.Drawing.Point(143, 313);
+            this.Price.Location = new System.Drawing.Point(138, 225);
+            this.Price.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Price.Name = "Price";
-            this.Price.Size = new System.Drawing.Size(100, 26);
+            this.Price.Size = new System.Drawing.Size(89, 22);
             this.Price.TabIndex = 8;
             this.Price.TextChanged += new System.EventHandler(this.Reisebüro_Load);
             // 
-            // tableLayoutPanel1
+            // flowLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.75717F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.24283F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 236F));
-            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(305, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(756, 790);
-            this.tableLayoutPanel1.TabIndex = 9;
-            this.tableLayoutPanel1.TabIndexChanged += new System.EventHandler(this.Reisebüro_Load);
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 125);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1147, 419);
+            this.flowLayoutPanel1.TabIndex = 9;
             // 
-            // listView1
+            // panel1
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.HotelName1,
-            this.Place1,
-            this.Price1});
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(3, 3);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(247, 784);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.List;
-            this.listView1.TabIndexChanged += new System.EventHandler(this.Reisebüro_Load);
+            this.panel1.Controls.Add(this.Picture);
+            this.panel1.Controls.Add(this.Place);
+            this.panel1.Controls.Add(this.Price);
+            this.panel1.Controls.Add(this.HotelName);
+            this.panel1.Controls.Add(this.TravelID);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(266, 328);
+            this.panel1.TabIndex = 10;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(15, 55);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(1144, 40);
+            this.flowLayoutPanel2.TabIndex = 10;
             // 
             // Reisebüro
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Teal;
-            this.ClientSize = new System.Drawing.Size(1061, 790);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.Price);
-            this.Controls.Add(this.TravelID);
-            this.Controls.Add(this.HotelName);
-            this.Controls.Add(this.Place);
-            this.Controls.Add(this.Picture);
+            this.ClientSize = new System.Drawing.Size(1171, 632);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Reisebüro";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reisebüro";
             ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -158,11 +153,9 @@
         private System.Windows.Forms.TextBox Place;
         private System.Windows.Forms.TextBox HotelName;
         private System.Windows.Forms.TextBox Price;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader HotelName1;
-        private System.Windows.Forms.ColumnHeader Place1;
-        private System.Windows.Forms.ColumnHeader Price1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
     }
 }
 
